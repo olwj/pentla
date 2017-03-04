7a5df6df8b0807b5e02667e4dc4961889a0e12cc
olwj <al.wajgert@gmail.com>
2017-03-04 13:30:06 +0100

    add final files

---
+++ src/com/sdajava/pentla_for_for/Main.java
@@ -0,0 +1,24 @@
+package com.sdajava.pentla_for_for;
+
+import java.security.cert.X509Certificate;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            for (int i = 1; i <= 4; i++) {
+                if(i==2 && i==3){
+                    System.out.printf("");
+                }
+
+                for (int j = 1; j <= 4; j++) {
+                    System.out.print("X");
+
+
+                }
+                System.out.println("");
+
+            }
+        }
+    }
